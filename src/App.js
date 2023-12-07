f81b278f821fc349b5eeaf930410de1f05e64721
Giantpro1 <shakiruolawale597@gmail.com>
2023-12-07 20:36:32 +0100

    navbar section

--- src/App.js
+++ src/App.js
@@ -1,51 +1,15 @@
 // import logo from './logo.svg';
 // import './App.css';
+import NavbarSection from './Navbar';
 import 'bootstrap/dist/css/bootstrap.css';
 // import  './images/airplane-seats-gce831a9cf_1280.jpg';
 import 'bootstrap/dist/js/bootstrap.bundle';
-import { Navbar, Container, NavDropdown, Nav, Form, Button, Row, Col, Card, Image } from 'react-bootstrap';
+import {Container, Button, Row, Col, Card, Image } from 'react-bootstrap';
 
 function App() {
   return (
     <>
-      <Navbar bg="dark" expand="lg" variant="dark" >
-        <Container>
-          <Navbar.Brand href="#">Close Shope</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: '100px' }}
-              navbarScroll
-            >
-              <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Link</Nav.Link>
-              <NavDropdown title="Link" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action5">
-                  Something else here
-                </NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="#" disabled>
-                Link
-              </Nav.Link>
-            </Nav>
-            <Form className="d-flex">
-              <Form.Control
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      < NavbarSection />
 
       <main>
         <Container className='mt-5'>
